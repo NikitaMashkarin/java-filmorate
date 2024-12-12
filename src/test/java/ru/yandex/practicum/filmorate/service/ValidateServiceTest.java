637d629a161dd69421bc95e1abe13c7fbd4887b2
NikitaMashkarin <mashkarinnikita@yandex.ru>
2024-12-12 21:05:24 +0300

    Исправить checkstyle

--- src/test/java/ru/yandex/practicum/filmorate/service/ValidateServiceTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/service/ValidateServiceTest.java
@@ -1,7 +1,6 @@
 package ru.yandex.practicum.filmorate.service;
 
 import org.junit.jupiter.api.Test;
-import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.repository.UserRepository;
